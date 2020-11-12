e998477747d06db18417e47e3cd09869f3ec5811
Alberto Cruz <albertocruzluis9@gmail.com>
2020-11-12 20:04:41 +0000

    Updates

---
+++ precache-manifest.8a3fcf511975c3062644c146d64c43ca.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a592f75fd9faf1e882f49ef5afdd5cd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96b059d4009cd4596594",
+    "url": "/static/css/main.27688b4b.chunk.css"
+  },
+  {
+    "revision": "42a25e1e613a3fcbb65f",
+    "url": "/static/js/2.5e5925c4.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/static/js/2.5e5925c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96b059d4009cd4596594",
+    "url": "/static/js/main.46eccd25.chunk.js"
+  },
+  {
+    "revision": "4db3ce52dad32e0b1e7f",
+    "url": "/static/js/runtime-main.6c51c7c6.js"
+  },
+  {
+    "revision": "f2f0daa4027db3c8e49dc29010d09621",
+    "url": "/static/media/Car.f2f0daa4.svg"
+  },
+  {
+    "revision": "ebf2f3f4fa6920c7ea512bd4c2cca145",
+    "url": "/static/media/Close.ebf2f3f4.svg"
+  },
+  {
+    "revision": "3b45ddf45b662c635c6b93d6e21f97a5",
+    "url": "/static/media/Cone.3b45ddf4.svg"
+  },
+  {
+    "revision": "c362294860d44c89eb969f172a1163c5",
+    "url": "/static/media/Finish.c3622948.svg"
+  },
+  {
+    "revision": "192a6620a687a9e7e51c0037973a1547",
+    "url": "/static/media/Github.192a6620.svg"
+  },
+  {
+    "revision": "eb46d6355c78e7a6324e9d0344330bf8",
+    "url": "/static/media/Info.eb46d635.svg"
+  },
+  {
+    "revision": "8414db1e87a0677b36f1d8514560b981",
+    "url": "/static/media/Logo.8414db1e.svg"
+  },
+  {
+    "revision": "5880fd532023bdf049486b59ae1d9f13",
+    "url": "/static/media/ManualExample.5880fd53.png"
+  },
+  {
+    "revision": "8d77e2c062318cadfb48d0720dcdd5f3",
+    "url": "/static/media/Upload.8d77e2c0.svg"
+  }
+]);
